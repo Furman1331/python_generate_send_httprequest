022d702d223bbbd5e45fdf1badfe6563288132c5
Furman1331 <michalfurmanczak@gmail.com>
2021-08-27 16:00:49 +0200

    end a post.

--- main.py
+++ main.py
@@ -9,35 +9,79 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def main():
-    data = createNewEvents(1)
-    r = requests.post('URL', data = data)
+print("Program testujący obciążenie bazy danych na serwerze.")
 
-def createNewEvents(count):
+def main(skipQuestion):
+    # Getting count of event and request from user.
+    count_events = input("wpisz ile wygenerować zdarzeń (default = 1): ")
+    count_request = input("wpisz ile wysłać takich requestów z tą samą ilością zdarzeń. (default = 1): ")
+
+    count_events = checkIsInt(count_events, 1)
+    count_request = checkIsInt(count_request, 1)
+
+    data = createNewEvents(count_events)
+    if(not skipQuestion):
+        passing = askUserBool("Wygenerowano zdarzenia, czy wysłać je na serwer ? (true/ false): ")
+
+        if(passing == 'true'):
+            sendRequest(data, count_request)
+            again = askUserBool("Czy wysłać zapytanie ponownie ? (true / false): ")
+            if(again == 'true'):
+                main(True)
+            else:
+                print("Exited.")
+        else:
+            print('Exited.')
+    else:
+        sendRequest(data, count_request)
+        again = askUserBool("Czy wysłać zapytanie ponownie ? (true / false): ")
+        if(again == 'true'):
+            main(True)
+        else:
+            print("Exited.")
+
+def sendRequest(data, count):
+    showFulldata = askUserBool("Czy pokazać odpowiedź serwera na zapytanie? (true / false): ")
+    for which in range(count):
+        try:
+            r = requests.post(os.environ.get('url'), data = data)
+
+            r.raise_for_status()
+        except HTTPError as http_err:
+            print(f'HTTP error occurred {http_err}')
+        except Exception as err:
+            print(f'Other error occurred {err}')
+        else:
+            print("Odpowiedz od serwera - Code Status: "+str(r.status_code)+" - Numer zapytania: "+str(which + 1)+" - Czas zapytania: "+str(r.elapsed.total_seconds()))
+            if(showFulldata == "true"):
+                print(r.json())
+
+
+def askUserBool(msg):
+    while True:
+        try:
+            passing = str(input(msg))
+            if(passing == 'true' or passing == 'false'):
+                break
+            else:
+                print("This command didn't match.")
+                continue
+        except ValueError:
+            print("This command didn't match.")
+            continue
+
+    return passing
+
+def createNewEvents(count_events):
     events = []
     # GENERATE A EVENT WITH A RANDOM COMMENT AND A DATETIME.
-    for _ in range(count):
-        events.append(json.dumps('{"comment":"'+get_random_string(10)+'","datetime":"'+random_date()+'","gps":"59.2545413;18.1161158","id":9,"location":"Tompol/sköndal","source":6,"status":0,"type":2}'))
+    for _ in range(count_events):
+        temp = {"comment":get_random_string(10),"datetime":random_date(),"gps":"59.2545413;18.1161158","id":8,"location":"Tompol/sköndal","source":6,"status":0,"type":2}
+        events.append(temp)
 
-    result = [
-        {
-            "key": "password",
-            "value": "YOUR PASSWORD",
-            "type": "text"
-        },
-        {
-            "key": "login",
-            "value": "YOUR LOGIN",
-            "type": "text"
-        },
-        {
-            "key": "events",
-            "value": json.dumps(events),
-            "type": "text"
-        }
-    ]
+    result = {"password" : str(os.environ.get('password')), "login" : str(os.environ.get('login')), "events" : json.dumps(events)}
 
-    return json.dumps(result)
+    return result
 
 def random_date():
     delta = datetime.strptime('1/1/2025 4:50 AM', '%m/%d/%Y %I:%M %p') - datetime.strptime('1/1/2022 1:30 PM', '%m/%d/%Y %I:%M %p')
@@ -51,9 +95,17 @@ def get_random_string(length):
     result_str = ''.join(random.choice(letters) for i in range(length))
     return result_str
 
+def checkIsInt(what, default):
+    try:
+        what = int(what)
+    except ValueError:
+        what = default
+
+    return what
+
 if(__name__) == '__main__':
     try:
-        main()
+        main(False)
     except MemoryError:
         sys.stderr.write("Maximum Memory Exceeded")
         sys.exit(-1)
